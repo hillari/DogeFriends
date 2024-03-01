60f38684fafe256244dec8ed31c543bb45b34a71
Hillari <hillari@gmail.com>
2024-03-01 12:50:33 -0900

    Added initial DbContext class skeleton. Added usings. AB#2

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
