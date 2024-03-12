6971e03c5b975def2fa30cfbe6ec0e5ec714faf0
Kristopher J. Carroll <kjcarroll@alaska.edu>
2024-03-12 13:19:59 -0800

    basic framework of Dog Entity AB#9

---
+++ API/Entities/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace API.Entities;
+
+public class Dog
+{
+    public int Id { get; set; }
+
+    public int Age { get; set; }
+
+    public string? Breed { get; set; }
+
+    public string? Color { get; set; }
+}
